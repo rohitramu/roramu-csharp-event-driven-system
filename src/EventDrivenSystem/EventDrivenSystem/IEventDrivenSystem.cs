98f5c7f0677d33e191304538e54dd887111cdcd1
Rohit Ramu <rohitramu@gmail.com>
2022-01-09 17:26:55 +1100

    Provide a read-only view of an event-driven system

--- src/EventDrivenSystem/EventDrivenSystem/IEventDrivenSystem.cs
+++ src/EventDrivenSystem/EventDrivenSystem/IEventDrivenSystem.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace RoRamu.EventDrivenSystem
 {
     /// <summary>
-    /// Represents an event-driven system.
+    /// Represents an event-driven system which can be updated.
     /// </summary>
     /// <typeparam name="T">
     /// The type which represents event ordering in this system.  For example, <see cref="int"/> to
@@ -17,56 +17,16 @@ namespace RoRamu.EventDrivenSystem
     /// <br/>
     /// Implementations will likely be more performant if objects of this type are mutable.
     /// </typeparam>
-    public interface IEventDrivenSystem<T, S>
+    public interface IEventDrivenSystem<T, S> : IReadOnlyEventDrivenSystem<T, S>
         where T : IEquatable<T>, IComparable<T>
         where S : IEquatable<S>
     {
         /// <summary>
-        /// The current state of this system.
-        /// </summary>
-        S CurrentState { get; }
-
-        /// <summary>
-        /// The state of the system when it was first created.
-        /// </summary>
-        S InitialState { get; }
-
-        /// <summary>
-        /// The time that the last event took place.
-        /// </summary>
-        T LastModifiedTimestamp { get; }
-
-        /// <summary>
-        /// The time that this system was created.  No events can be added before this time.
-        /// </summary>
-        T CreatedTimestamp { get; }
-
-        /// <summary>
-        /// Gets all events in chronological order.
-        /// </summary>
-        /// <returns>All events.</returns>
-        IEnumerable<IEvent<T, S>> GetEvents();
-
-        /// <summary>
-        /// Gets events in chronological order from the given time.
-        /// </summary>
-        /// <param name="from">The earliest time to start searching for events.</param>
-        /// <returns>All events from the specified time.</returns>
-        IEnumerable<IEvent<T, S>> GetEvents(T from);
-
-        /// <summary>
-        /// Gets events in chronological order in the given time window.
-        /// </summary>
-        /// <param name="from">The start of the time window (inclusive).</param>
-        /// <param name="to">The end of the time window (exclusive).</param>
-        /// <returns>All events in the specified time window.</returns>
-        IEnumerable<IEvent<T, S>> GetEvents(T from, T to);
-
-        /// <summary>
         /// Adds the event to the system's history, and updates the current state if necessary.
         /// </summary>
         /// <exception cref="ArgumentException">
-        /// Thrown when the given event has a timestamp that comes before <see cref="CreatedTimestamp"/>.
+        /// Thrown when the given event has a timestamp that comes before
+        /// <see cref="IReadOnlyEventDrivenSystem{T, S}.CreatedTimestamp"/>.
         /// </exception>
         /// <param name="toAdd">The event to add.</param>
         /// <returns>True if the event was successfully added, otherwise false.</returns>
@@ -78,18 +38,5 @@ namespace RoRamu.EventDrivenSystem
         /// <param name="toRemove">The event to remove.</param>
         /// <returns>True if the event was successfully removed, otherwise false.</returns>
         bool RemoveEvent(IEvent<T, S> toRemove);
-
-        /// <summary>
-        /// Gets the state of the system at the given time, based on the history of events.
-        /// </summary>
-        /// <param name="timestamp">The time at which to get the state of the system.</param>
-        /// <returns>
-        /// The state of the system at the given time, otherwise null if the given time preceeds
-        /// the existence of this system (i.e. the system has not yet been created).
-        /// <br/>
-        /// If the given time is later than the last modified time of this system, the current state
-        /// will be returned.
-        /// </returns>
-        S GetStateAtTimestamp(T timestamp);
     }
 }
---
+++ src/EventDrivenSystem/EventDrivenSystem/IReadOnlyEventDrivenSystem.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+
+namespace RoRamu.EventDrivenSystem
+{
+    /// <summary>
+    /// Represents an event-driven system.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type which represents event ordering in this system.  For example, <see cref="int"/> to
+    /// represent sequence numbers, or <see cref="DateTime"/> to represent time.
+    /// <br/>
+    /// Objects of this type should be immutable.
+    /// </typeparam>
+    /// <typeparam name="S">
+    /// The type which represents the state of the system.
+    /// <br/>
+    /// Implementations will likely be more performant if objects of this type are mutable.
+    /// </typeparam>
+    public interface IReadOnlyEventDrivenSystem<T, S>
+        where T : IEquatable<T>, IComparable<T>
+        where S : IEquatable<S>
+    {
+        /// <summary>
+        /// The current state of this system.
+        /// </summary>
+        S CurrentState { get; }
+
+        /// <summary>
+        /// The state of the system when it was first created.
+        /// </summary>
+        S InitialState { get; }
+
+        /// <summary>
+        /// The time that the last event took place.
+        /// </summary>
+        T LastModifiedTimestamp { get; }
+
+        /// <summary>
+        /// The time that this system was created.  No events can be added before this time.
+        /// </summary>
+        T CreatedTimestamp { get; }
+
+        /// <summary>
+        /// Gets all events in chronological order.
+        /// </summary>
+        /// <returns>All events.</returns>
+        IEnumerable<IEvent<T, S>> GetEvents();
+
+        /// <summary>
+        /// Gets events in chronological order from the given time.
+        /// </summary>
+        /// <param name="from">The earliest time to start searching for events.</param>
+        /// <returns>All events from the specified time.</returns>
+        IEnumerable<IEvent<T, S>> GetEvents(T from);
+
+        /// <summary>
+        /// Gets events in chronological order in the given time window.
+        /// </summary>
+        /// <param name="from">The start of the time window (inclusive).</param>
+        /// <param name="to">The end of the time window (exclusive).</param>
+        /// <returns>All events in the specified time window.</returns>
+        IEnumerable<IEvent<T, S>> GetEvents(T from, T to);
+
+        /// <summary>
+        /// Gets the state of the system at the given time, based on the history of events.
+        /// </summary>
+        /// <param name="timestamp">The time at which to get the state of the system.</param>
+        /// <returns>
+        /// The state of the system at the given time, otherwise null if the given time preceeds
+        /// the existence of this system (i.e. the system has not yet been created).
+        /// <br/>
+        /// If the given time is later than the last modified time of this system, the current state
+        /// will be returned.
+        /// </returns>
+        S GetStateAtTimestamp(T timestamp);
+    }
+}
